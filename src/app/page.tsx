cb1cdcee7d5ee8926e12a17519c47abc453b31ab
SuwasG <suwasghale693@gmail.com>
2025-09-22 06:59:06 +0545

    improved folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import FeaturedBrands from "@/components/Brands";
 import Category from "@/components/Category";
 import HeroSection from "@/components/Hero";
-import LaptopGrid from "@/components/LaptopCard";
+import LaptopGrid from "@/features/products/components/LaptopCard";
 import TrustSection from "@/components/TrustAndSupport";
 
 export default function Home() {
