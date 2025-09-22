eabcb37a9387c09fdacf6fe2a7272f0ef0a9658b
SuwasG <suwasghale693@gmail.com>
2025-09-22 14:58:12 +0545

    product grid added

---
+++ src/features/products/components/ProductGrid.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import ProductCard from "./ProductCard";
+import type { Product } from "../types/product";
+
+interface ProductGridProps {
+  products: Product[];
+}
+
+const ProductGrid = ({ products }: ProductGridProps) => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductGrid;
